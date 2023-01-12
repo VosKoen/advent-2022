77bfdfc37012323113627d43f6db0f0071b1e6e1
Koen de Vos <koen@MacBook-Pro-van-Koen-2.local>
2023-01-12 21:10:15 +0100

    day 20

--- day-20/functions.js
+++ day-20/functions.js
@@ -16,13 +16,58 @@ exports.runB = exports.runA = void 0;
 const parseInput_1 = __importDefault(require("../common/parseInput"));
 function runA() {
     return __awaiter(this, void 0, void 0, function* () {
-        const inputArray = yield (0, parseInput_1.default)(__dirname + "/input");
+        const original = (yield (0, parseInput_1.default)(__dirname + "/input")).map((line) => parseInt(line, 10));
+        const dynamicFile = original.map((value, originalIndex) => ({
+            value,
+            originalIndex,
+        }));
+        original.forEach((item, index) => {
+            const currentIndex = dynamicFile.findIndex((entry) => entry.originalIndex === index);
+            const file = dynamicFile.splice(currentIndex, 1)[0];
+            let newIndex;
+            if (currentIndex + file.value < 0) {
+                newIndex = dynamicFile.length + (currentIndex + file.value);
+            }
+            else {
+                newIndex = (currentIndex + file.value) % dynamicFile.length;
+            }
+            dynamicFile.splice(newIndex, 0, file);
+        });
+        const startIndex = dynamicFile.findIndex((item) => item.value === 0);
+        const firstValue = dynamicFile[(1000 + startIndex) % dynamicFile.length].value;
+        const secondValue = dynamicFile[(2000 + startIndex) % dynamicFile.length].value;
+        const thirdValue = dynamicFile[(3000 + startIndex) % dynamicFile.length].value;
+        console.log(firstValue + secondValue + thirdValue);
     });
 }
 exports.runA = runA;
 function runB() {
     return __awaiter(this, void 0, void 0, function* () {
-        const inputArray = yield (0, parseInput_1.default)(__dirname + "/input");
+        const original = (yield (0, parseInput_1.default)(__dirname + "/input")).map((line) => parseInt(line, 10) * 811589153);
+        const dynamicFile = original.map((value, originalIndex) => ({
+            value,
+            originalIndex,
+        }));
+        for (let i = 0; i < 10; i++) {
+            original.forEach((item, index) => {
+                const currentIndex = dynamicFile.findIndex((entry) => entry.originalIndex === index);
+                const file = dynamicFile.splice(currentIndex, 1)[0];
+                let newIndex;
+                if (currentIndex + file.value < 0) {
+                    newIndex =
+                        dynamicFile.length + ((currentIndex + file.value) % dynamicFile.length);
+                }
+                else {
+                    newIndex = (currentIndex + file.value) % dynamicFile.length;
+                }
+                dynamicFile.splice(newIndex, 0, file);
+            });
+        }
+        const startIndex = dynamicFile.findIndex(item => item.value === 0);
+        const firstValue = dynamicFile[(1000 + startIndex) % dynamicFile.length].value;
+        const secondValue = dynamicFile[(2000 + startIndex) % dynamicFile.length].value;
+        const thirdValue = dynamicFile[(3000 + startIndex) % dynamicFile.length].value;
+        console.log(firstValue + secondValue + thirdValue);
     });
 }
 exports.runB = runB;
--- day-20/functions.ts
+++ day-20/functions.ts
@@ -1,9 +1,68 @@
 import parseInput from "../common/parseInput";
 
 export async function runA() {
-    const inputArray = await parseInput(__dirname + "/input");
+  const original = (await parseInput(__dirname + "/input")).map((line) =>
+    parseInt(line, 10)
+  );
+  const dynamicFile = original.map((value, originalIndex) => ({
+    value,
+    originalIndex,
+  }));
+  original.forEach((item, index) => {
+    const currentIndex = dynamicFile.findIndex(
+      (entry) => entry.originalIndex === index
+    );
+    const file = dynamicFile.splice(currentIndex, 1)[0];
+    let newIndex: number;
+    if (currentIndex + file.value < 0) {
+      newIndex = dynamicFile.length + (currentIndex + file.value);
+    } else {
+      newIndex = (currentIndex + file.value) % dynamicFile.length;
+    }
+    dynamicFile.splice(newIndex, 0, file);
+  });
+
+  const startIndex = dynamicFile.findIndex((item) => item.value === 0);
+
+  const firstValue =
+    dynamicFile[(1000 + startIndex) % dynamicFile.length].value;
+  const secondValue =
+    dynamicFile[(2000 + startIndex) % dynamicFile.length].value;
+  const thirdValue =
+    dynamicFile[(3000 + startIndex) % dynamicFile.length].value;
+
+  console.log(firstValue + secondValue + thirdValue);
 }
 
 export async function runB() {
-    const inputArray = await parseInput(__dirname + "/input");
+  const original = (await parseInput(__dirname + "/input")).map(
+    (line) => parseInt(line, 10) * 811589153
+  );
+  const dynamicFile = original.map((value, originalIndex) => ({
+    value,
+    originalIndex,
+  }));
+  for(let i = 0; i < 10; i++) {
+    original.forEach((item, index) => {
+      const currentIndex = dynamicFile.findIndex(
+          (entry) => entry.originalIndex === index
+      );
+      const file = dynamicFile.splice(currentIndex, 1)[0];
+      let newIndex: number;
+      if (currentIndex + file.value < 0) {
+        newIndex =
+            dynamicFile.length + ((currentIndex + file.value) % dynamicFile.length);
+      } else {
+        newIndex = (currentIndex + file.value) % dynamicFile.length;
+      }
+      dynamicFile.splice(newIndex, 0, file);
+    });
+  }
+  const startIndex = dynamicFile.findIndex(item => item.value === 0);
+
+  const firstValue = dynamicFile[(1000+startIndex) % dynamicFile.length].value;
+  const secondValue = dynamicFile[(2000+startIndex) % dynamicFile.length].value;
+  const thirdValue = dynamicFile[(3000+startIndex) % dynamicFile.length].value;
+
+  console.log(firstValue + secondValue + thirdValue);
 }
